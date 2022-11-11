14eb547bdc93639255d464e15ad530a59a5665f1
Jawad Qureshi <qureshi@uchicago.edu>
2022-11-11 17:23:22 -0600

    Big push

---
+++ overrides.sh
@@ -0,0 +1,6 @@
+[[ -e /overrides/gitops-logo.png ]] && cp /overrides/gitops-logo.png /data-portal/custom/logo/gitops-logo.png
+[[ -e /overrides/gitops.json ]] && cp /overrides/gitops.json /data-portal/data/config/gitops.json
+[[ -e /overrides/gitops-logo.png ]] && cp /overrides/gitops-logo.png /data-portal/custom/logo/gitops-logo.png
+[[ -e /overrides/gitops-favicon.ico ]] && cp /overrides/gitops-favicon.ico /data-portal/custom/favicon/gitops-favicon.ico
+[[ -e /overrides/gitops.css ]] && cp /overrides/gitops.css /data-portal/custom/css/gitops.css
+[[ -e /overrides/gitops-sponsors/ ]] && cp -r /overrides/gitops-sponsors /data-portal/custom/sponsors/gitops-sponsors
